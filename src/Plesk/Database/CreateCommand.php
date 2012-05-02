d0ea6af4ca613866be835b8b00b7f337be1943c3
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2012-05-02 09:56:50 -0500

    updated composer.json and added some plesk utilities

---
+++ src/Plesk/Database/CreateCommand.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Plesk\Database;
+
+/**
+ * Description
+ *
+ * @package
+ * @subpackage
+ * @author     Joshua Estes
+ * @copyright  2012
+ * @version    0.1.0
+ * @category
+ * @license
+ *
+ */
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+if (!defined('PLESK_BIN')) {
+    define('PLESK_BIN', '/usr/local/psa/bin');
+}
+
+class CreateCommand extends Command {
+
+    protected function configure() {
+        $this
+            ->setName('plesk:database:create')
+            ->setDescription('Create a new database');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $cmd = array();
+
+        /**
+         * Required parameters that need to be filled in by user
+         */
+        do {
+            $database_name = $this->getDialog()->ask($output, '<question>Database Name </question>: ');
+        }
+        while (!$database_name);
+        $cmd[] = sprintf('--create "%s"', $database_name);
+
+        do {
+            $domain = $this->getDialog()->ask($output, '<question>Domain (ie: beta.example.com)</question>: ');
+        }
+        while (!$domain);
+        $cmd[] = sprintf('-domain "%s"', $domain);
+
+        do {
+            $user = $this->getDialog()->ask($output, '<question>Database User</question>: ');
+        }
+        while (!$user);
+        $cmd[] = sprintf('-add_user "%s"', $user);
+
+        do {
+            $passwd = $this->getDialog()->ask($output, '<question>Database User Password</question>: ');
+        }
+        while (!$passwd);
+        $cmd[] = sprintf('-passwd "%s"', $passwd);
+
+
+        /**
+         * Optional stuff
+         */
+        $type = $this->getDialog()->ask($output, '<question>Type (default: mysql)</question>: ', 'mysql');
+        $cmd[] = sprintf('-type "%s"', $type);
+
+        $command = \implode(" ", $cmd);
+        $process = new Process(sprintf('%s/database %s', \PLESK_BIN, $command));
+        $process->run(function($type, $buffer) use($output) {
+                $output->writeln($buffer);
+            }
+        );
+    }
+
+    /**
+     *
+     * @return Symfony\Component\Console\Helper\DialogHelper
+     */
+    protected function getDialog() {
+        return $this->getHelperSet()->get('dialog');
+    }
+
+}
---
+++ src/Plesk/Subscription/CreateCommand.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Plesk\Subscription;
+
+/**
+ * Description
+ *
+ * @package
+ * @subpackage
+ * @author     Joshua Estes
+ * @copyright  2012
+ * @version    0.1.0
+ * @category
+ * @license
+ *
+ */
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+if (!defined('PLESK_BIN')) {
+    define('PLESK_BIN', '/usr/local/psa/bin');
+}
+
+class CreateCommand extends Command {
+
+    protected function configure() {
+        $this
+            ->setName('plesk:subscription:create')
+            ->setDescription('Create a new subscription');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $cmd = array();
+        do {
+            $subscription_name = $this->getDialog()->ask($output, '<question>Subscription Name (ie: example.com)</question>: ');
+        }
+        while (!$subscription_name);
+        $cmd[] = sprintf('--create "%s"', $subscription_name);
+
+        do {
+            $owner = $this->getDialog()->ask($output, '<question>Owner (ie: login_name)</question>: ');
+        }
+        while (!$owner);
+        $cmd[] = sprintf('-owner "%s"', $owner);
+
+        do {
+            $ip = $this->getDialog()->ask($output, '<question>IP Address (ie: The public IP of the server)</question>: ');
+        }
+        while (!$ip);
+        $cmd[] = sprintf('-ip "%s"', $ip);
+
+        $service_plan = $this->getDialog()->ask($output, '<question>Service Plan (default: Default Domain)</question>: ', 'Default Domain');
+        $cmd[] = sprintf('-service-plan "%s"', $service_plan);
+
+        $hosting = $this->getDialog()->askConfirmation($output, '<question>Enable Hosting (default: y)</question>: ', true);
+        $cmd[] = sprintf('-hosting %s', $hosting ? 'true' : 'false');
+        if ($hosting) {
+            do {
+                $login = $this->getDialog()->ask($output, '<question>Username for loggin into services</question>: ');
+            }
+            while (!$login);
+            $cmd[] = sprintf('-login "%s"', $login);
+
+            do {
+                $passwd = $this->getDialog()->ask($output, '<question>Password for user</question>: ');
+            }
+            while (!$passwd);
+            $cmd[] = sprintf('-passwd "%s"', $passwd);
+        }
+
+        $command = \implode(" ", $cmd);
+        $process = new Process(sprintf('%s/subscription %s', \PLESK_BIN, $command));
+        $process->run(function($type, $buffer) use($output) {
+                $output->writeln($buffer);
+            }
+        );
+    }
+
+    /**
+     *
+     * @return Symfony\Component\Console\Helper\DialogHelper
+     */
+    protected function getDialog() {
+        return $this->getHelperSet()->get('dialog');
+    }
+
+}
