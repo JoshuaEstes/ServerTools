09a8280148d00a808641d3d1a645efd7fff459d4
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2012-05-07 21:11:09 -0500

    working on writing tests

---
+++ Tests/Etc/Hosts/AddCommandTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Test\Etc\Hosts;
+
+/**
+ * Description
+ * 
+ * @author      Joshua Estes
+ * @copyright
+ * @package
+ * @subpackage
+ * @version
+ */
+use Symfony\Component\Console\Application;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Console\Tester\CommandTester;
+use Etc\Hosts\AddCommand;
+
+class AddCommandTest extends \PHPUnit_Framework_TestCase
+{
+
+  public function testExecute()
+  {
+    $hostsfile = '/tmp/' . \time();
+    // Create the file
+    $filesystem = new Filesystem();
+    $filesystem->touch($hostsfile);
+
+    $application = new Application();
+    $application->add(new AddCommand());
+
+    $command = $application->find('etc:hosts:add');
+    $commandTester = new CommandTester($command);
+    $commandTester->execute(
+      array(
+        'command' => $command->getName(),
+        '--hostname' => 'test.local',
+        '--hosts-file' => $hostsfile,
+      ),
+      array(
+        'interactive' => false,
+//        'decorated' => true,
+        'verbosity' => true
+      )
+    );
+
+    $contents = \file_get_contents($hostsfile);
+
+    $this->assertContains("127.0.0.1 test.local", $contents);
+
+    // remove the file
+    $filesystem->remove($hostsfile);
+  }
+
+}
---
+++ Tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description
+ * 
+ * @author      Joshua Estes
+ * @copyright
+ * @package
+ * @subpackage
+ * @version
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
