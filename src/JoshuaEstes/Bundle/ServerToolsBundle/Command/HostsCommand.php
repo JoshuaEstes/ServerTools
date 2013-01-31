55fced53cf554a349a2524ef79c4c6deaf0fc31d
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2013-01-31 09:52:21 -0600

    updated the hosts command with some options and such, will add more stuff later

--- src/JoshuaEstes/Bundle/ServerToolsBundle/Command/HostsCommand.php
+++ src/JoshuaEstes/Bundle/ServerToolsBundle/Command/HostsCommand.php
@@ -16,7 +16,31 @@ class HostsCommand extends Command
         $this
             ->setName('hosts')
             ->setDescription('Manage your /etc/hosts file')
+            ->setDefinition(array(
+                new InputOption('file', 'f', InputOption::VALUE_REQUIRED, 'Location of your hosts file', '/etc/hosts'),
+                new InputOption('list', 'l', InputOption::VALUE_NONE, 'List all of the known hosts in your hosts file'),
+                new InputOption('edit', 'E', InputOption::VALUE_NONE, 'Open up the hosts file in your editor'),
+                new InputOption('add', 'a', InputOption::VALUE_NONE, 'Add an entry to your hosts file'),
+                new InputOption('delete', 'D', InputOption::VALUE_NONE, 'Remove an entry in your hosts file'),
+                new InputOption('enable', 'e', InputOption::VALUE_NONE, 'Enable a host'),
+                new InputOption('disable', 'd', InputOption::VALUE_NONE, 'Disable a host'),
+            ))
+            ->setHelp(<<<EOF
+EOF
+            )
         ;
+    }
+
+    /**
+     * Find the location of the hosts file based on the users
+     * platform
+     */
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
     }
 
 }
