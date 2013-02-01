cf0f73e24f0138fe8aac65119aeef1f8d8ab83e2
Joshua Estes <Joshua.Estes@iostudio.com>
2013-02-01 09:19:55 -0600

    updated the command a little, can list and add entries

--- src/JoshuaEstes/Bundle/ServerToolsBundle/Command/HostsCommand.php
+++ src/JoshuaEstes/Bundle/ServerToolsBundle/Command/HostsCommand.php
@@ -8,6 +8,11 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Command to manage your hosts file.
+ *
+ * @author Joshua Estes
+ */
 class HostsCommand extends Command
 {
 
@@ -24,6 +29,8 @@ class HostsCommand extends Command
                 new InputOption('delete', 'D', InputOption::VALUE_NONE, 'Remove an entry in your hosts file'),
                 new InputOption('enable', 'e', InputOption::VALUE_NONE, 'Enable a host'),
                 new InputOption('disable', 'd', InputOption::VALUE_NONE, 'Disable a host'),
+                new InputOption('ip', null, InputOption::VALUE_REQUIRED, 'IP Address', '127.0.0.1'),
+                new InputArgument('host', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'hostname or list of hostnames'),
             ))
             ->setHelp(<<<EOF
 EOF
@@ -40,6 +47,79 @@ EOF
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $hostsFile = $input->getOption('file');
+
+        /**
+         * List entries in the hosts file
+         */
+        if ($input->getOption('list')) {
+            $handle = fopen($hostsFile, 'r');
+            while(false !== ($buffer = fgets($handle, 1024))) {
+                $buffer = trim($buffer);
+                if (empty($buffer)) { continue; }
+                // split based on tabs, spaces
+                $buffer    = preg_split('/\s/', $buffer);
+                $ip        = $buffer[0];
+                $enabled   = true;
+                $hosts     = array();
+                $hostsLine = '';
+                if (preg_match('/^#/', $ip)) {
+                    $enabled = false;
+                    $ip      = preg_replace('/^#/', '', $ip);
+                }
+
+                for($i=1;$i<count($buffer);$i++) {
+                    $hosts[] = $buffer[$i];
+                    $hostsLine .= $buffer[$i] . ($i<count($buffer)-1 ? ', ' : '');
+                }
+                $output->writeln(array(
+                    sprintf('IP Address: %s', $ip),
+                    sprintf('Enabled:    %s', ($enabled ? 'Yes' : 'No')),
+                    sprintf('Hosts:      %s', $hostsLine),
+                    '',
+                ));
+            }
+            fclose($handle);
+        }
+
+        /**
+         * Add an entry in the hosts file
+         */
+        if ($input->getOption('add')) {
+            // Make sure we have an ip address and at least one host
+            if (!$input->getOption('ip') || count($input->getArgument('host')) <= 0) {
+                throw new \Exception('Need both IP address and at least one host');
+            }
+            // check to see if we can write to the host file
+            if (!is_writable($hostsFile)) {
+                throw new \Exception(sprintf('Can not write to file "%s". Try `sudo !!`', $hostsFile));
+            }
+            $line = $input->getOption('ip') . "\t";
+            foreach ($input->getArgument('host') as $v) {
+                $line .= $v . "\t";
+            }
+
+            if ($input->isInteractive()) {
+                $dialog = $this->getHelperSet()->get('dialog');
+                $output->writeln(array(
+                    $line,
+                ));
+                if (!$dialog->askConfirmation($output, 'Do you confirm? [Y/n]', true)) {
+                    $output->writeln('Command Aborted');
+                    return 1;
+                }
+            }
+
+            $handler = fopen($hostsFile, 'a');
+            fwrite($handler, trim($line) . "\n");
+            fclose($handler);
+        }
+    }
+
+    /**
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
     {
     }
 
